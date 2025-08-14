e3392975a9d3370b97dfeb83e58b1ab1ecf2ad38
Peter Neumark <neumark.peter@gmail.com>
2025-08-14 16:52:21 +0200

    proof of concept mix html + react rendering mdx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,29 @@
-import "/styles.css"
+import type { JSX } from "react";
+import React, { Suspense } from "react";
+import { SandboxRouter, Path } from "@tinkerable/internal/v1";
+import { Routes, Route } from "react-router";
+
+export const Toc = (): JSX.Element => {
+  console.log("ASDFASDF");
+  const MyTOC = React.lazy(() => module.dynamicImport("./pages/toc.mdx", "*"));
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <section>
+        <MyTOC />
+      </section>
+    </Suspense>
+  );
+};
+
+export const App = (): React.JSX.Element => {
+  return (
+    <SandboxRouter>
+      <Routes>
+        <Route index element={<Toc />} />
+        {/* Treat index.tsx as a special case to avoid circular imports */}
+        <Route path="/index.tsx" element={<Toc />} />
+        <Route path="*" element={<Path />} />
+      </Routes>
+    </SandboxRouter>
+  );
+};
---
+++ index.tsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+import { App } from "./App";
+import { boot } from "@tinkerable/internal/v1";
+
+boot({ App, components: {} });
